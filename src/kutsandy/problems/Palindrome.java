0497963145805e92fdfc0e744623236f8344bfb8
Andy1945 <kuts1945@gmail.com>
2023-03-03 16:53:32 +0300

    Palindrome

---
+++ src/kutsandy/problems/Palindrome.java
@@ -0,0 +1,13 @@
+package kutsandy.problems;
+/*Determine whether an integer is a palindrome.
+An integer is a palindrome when it reads the same
+backwards as forward.
+
+Solve without converting the number to a string
+*/
+
+public class Palindrome {
+    public static boolean isPalindrome(int x){
+
+    }
+}
