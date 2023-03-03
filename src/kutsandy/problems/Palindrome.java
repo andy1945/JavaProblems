87475aaf9e2067ba2b71a1b7957d7dd8c0120b41
Andy1945 <kuts1945@gmail.com>
2023-03-03 17:14:18 +0300

    Palindrome

--- src/kutsandy/problems/Palindrome.java
+++ src/kutsandy/problems/Palindrome.java
@@ -8,6 +8,21 @@ Solve without converting the number to a string
 
 public class Palindrome {
     public static boolean isPalindrome(int x){
+       //check if its not a negative
+        if (x < 0) return false;
+        int reverse = 0, copy =x;
+        while (copy > 0){
+            int digit = copy % 10;
+            reverse =  reverse *10 + digit;
+            copy /= 10;
+        }
+        return reverse ==x;
+
+    }
+
+    public static void main(String args[]){
+        int p = 4832;
+        System.out.println(isPalindrome(p));
 
     }
 }
